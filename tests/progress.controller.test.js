7f6e4518649cdeaa2a7379846998eac070ccbbd9
Wesley Chu <wwc2113@columbia.edu>
2016-04-03 11:16:52 -0400

    controller unit tests

--- tests/progress.controller.test.js
+++ tests/progress.controller.test.js
@@ -1,9 +1,19 @@
 describe ('progress', function(){
 	beforeEach(module('CircleProgressApp'));
+	
 	var $controller;
 	beforeEach(inject(function(_$controller_){
 		$controller = _$controller_;
 		}));
+	describe('diff', function(){
+		it('basic subtraction', function(){
+			var $scope = {};
+			var controller = $controller('ProgressController',{$scope: $scope});
+			$scope.data.actual = 0.7;
+			$scope.data.expected = 0.1;
+			expect($scope.data.diff()).toBe(0.6);
+		});
+	});
 
 	describe('color', function(){
 		it('50% behind should be red', function(){
